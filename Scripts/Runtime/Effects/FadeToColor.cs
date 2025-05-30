483e7ee166506887cf7a684bfc729a3c6e2ac991
davidplace1 <david.t.place@gmail.com>
2025-05-30 17:15:34 +0100

    Renamed 'Color Fade' effect to 'Fade To Color'

--- Scripts/Runtime/Effects/FadeToColor.cs
+++ Scripts/Runtime/Effects/FadeToColor.cs
@@ -4,27 +4,27 @@ using UnityEngine.Rendering;
 namespace Nexcide.PostProcessing {
 
     [VolumeComponentMenu("Nexcide/Color Fade")]
-    public class ColorFade : VolumeComponentBase {
+    public class FadeToColor : VolumeComponentBase {
 
-        public ClampedFloatParameter Amount = new(0.0f, 0.0f, 1.0f);
+        public ClampedFloatParameter Blend = new(0.0f, 0.0f, 1.0f);
         public ColorParameter Color = new(UnityEngine.Color.white, hdr: true, showAlpha: false, showEyeDropper: false);
 
-        public override bool IsActive() => (Amount.value > 0.0f);
+        public override bool IsActive() => (Blend.value > 0.0f);
     }
 
-    [PostProcessEffect(typeof(ColorFade))]
-    public class ColorFadeEffect : VolumeEffect {
+    [PostProcessEffect(typeof(FadeToColor))]
+    public class FadeToColorEffect : VolumeEffect {
 
-        public override string ShaderName => "Nexcide/Color Fade";
+        public override string ShaderName => "Nexcide/Fade To Color";
 
-        private static readonly int _amount = Shader.PropertyToID("_Amount");
+        private static readonly int _blend = Shader.PropertyToID("_Blend");
         private static readonly int _color = Shader.PropertyToID("_Color");
 
         public override bool ConfigureMaterial(VolumeStack volumeStack, MaterialPropertyBlock material) {
-            bool active = ComponentActive(volumeStack, out ColorFade component);
+            bool active = ComponentActive(volumeStack, out FadeToColor component);
 
             if (active) {
-                material.SetFloat(_amount, component.Amount.value);
+                material.SetFloat(_blend, component.Blend.value);
                 material.SetColor(_color, component.Color.value);
             }
 
