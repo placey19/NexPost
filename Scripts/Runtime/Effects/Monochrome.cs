3c75a3e9cc503c0a69b2ca227e384d2bbefdf9ef
davidplace1 <david.t.place@gmail.com>
2025-07-01 19:15:03 +0100

    Added Color parameter to Monochrome shader.

--- Scripts/Runtime/Effects/Monochrome.cs
+++ Scripts/Runtime/Effects/Monochrome.cs
@@ -5,8 +5,9 @@ namespace Nexcide.PostProcessing {
 
     [VolumeComponentMenu("Nexcide/Monochrome")]
     public class Monochrome : VolumeComponentBase {
-    
+
         public ClampedFloatParameter Blend = new(0.0f, 0.0f, 1.0f);
+        public NoInterpColorParameter Color = new(UnityEngine.Color.white);
         public BoolParameter InvertColors = new(false);
         public NoInterpClampedFloatParameter Edge = new(0.65f, 0.0f, 1.0f);
 
@@ -17,8 +18,9 @@ namespace Nexcide.PostProcessing {
     public class MonochromeEffect : VolumeEffect {
 
         public override string ShaderName => "Nexcide/Monochrome";
-
+        
         private static readonly int _blend = Shader.PropertyToID("_Blend");
+        private static readonly int _color = Shader.PropertyToID("_Color");
         private static readonly int _invertColors = Shader.PropertyToID("_InvertColors");
         private static readonly int _edge = Shader.PropertyToID("_Edge");
 
@@ -27,6 +29,7 @@ namespace Nexcide.PostProcessing {
 
             if (active) {
                 material.SetFloat(_blend, component.Blend.value);
+                material.SetColor(_color, component.Color.value);
                 material.SetFloat(_invertColors, component.InvertColors.value ? 1.0f : 0.0f);
                 material.SetFloat(_edge, component.Edge.value);
             }
